937ee96febdd86f58e1a6d88d91c48569243b4e8
Andrii Nalyaiko <videoedit.promo@gmail.com>
2024-02-25 09:02:37 +0000

    Up

--- js/index.js
+++ js/index.js
@@ -7,12 +7,30 @@
  *? інакше відобразити:"Не знаєте? ECMAScript!"
  */
 
-function checkAnsfer () {
-    const string = prompt("Яка офіційна назва JavaScript?")
-    if (string === "ECMAScript") {
-        alert("Вірно!")
-    } else {
-        alert("Не знаєте? ECMAScript!")
-    }
+// function checkAnsfer () {
+//     const string = prompt("Яка офіційна назва JavaScript?")
+//     if (string === "ECMAScript") {
+//         alert("Вірно!")
+//     } else {
+//         alert("Не знаєте? ECMAScript!")
+//     }
+// }
+// checkAnsfer();
+
+/**
+ *? У змінній min лежить число від 0 до 59.
+ *? Визначте, в яку чверть години потрапляє
+ *? це число (у першу, другу, третю чи четверту).
+ 
+ */
+
+const min = 100;
+if (min <= 15) {
+  alert(1);
+} else if (min >= 16 && min <= 30) {
+  alert(2);
+} else if (min >= 31 && min <= 45) {
+  alert(3);
+} else {
+  alert(4);
 }
-checkAnsfer();
